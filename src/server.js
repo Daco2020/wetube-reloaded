87a4207691c594ea34ab386ee4dfea928c1d2ad8
Daco <dacokim32@gmail.com>
2021-05-02 19:26:19 +0900

    #15.1 GET Requests

---
+++ src/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const PORT = 5289;
+
+const app = express();
+
+const handleListening = () =>
+  console.log("Server Listenting on port http://localhost:5289");
+
+app.listen(PORT, handleListening);
+
+//callback? -> 서버가 실행되었을때 시작하는 함수?
+// npm run dev -> 노드맨 터미널 실행
