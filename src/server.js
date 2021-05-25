67705a559314d761e8299e8cb9af82090dfe20c2
Daco <dacokim32@gmail.com>
2021-05-25 23:22:46 +0900

    #3.2 GET Requests part Two

--- src/server.js
+++ src/server.js
@@ -1,13 +1,18 @@
 import express from "express";
 
-const PORT = 7237;
+const PORT = 5289;
 
 const app = express();
 
+const handleHome = () => console.log("go to home");
+
+app.get("/", handleHome);
+
 const handleListening = () =>
-  console.log("Server Listenting on port http://localhost:5289");
+  console.log(`Server Listenting on port http://localhost:${PORT}`);
 
 app.listen(PORT, handleListening);
 
 //callback? -> 서버가 실행되었을때 시작하는 함수?
 // npm run dev -> 노드맨 터미널 실행
+// http://localhost:5289/ 서버 주소
