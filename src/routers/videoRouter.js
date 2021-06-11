6560b06aa746536b898f8a84c2f36253097f06ef
Daco <dacokim32@gmail.com>
2021-06-11 23:36:14 +0900

    #4.8 URL Parameters part Two

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -4,12 +4,16 @@ import { see, edit, upload, deleteVideo } from "../controllers/videoController";
 const videoRouter = express.Router();
 
 videoRouter.get("/upload", upload);
-videoRouter.get("/:id", see);
-videoRouter.get("/:id/edit", edit);
-videoRouter.get("/:id/delete", deleteVideo);
+videoRouter.get("/:id(\\d+)", see);
+videoRouter.get("/:id(\\d+)/edit", edit);
+videoRouter.get("/:id(\\d+)/delete", deleteVideo);
 
 export default videoRouter;
 
 // : > 파라미터 > url 안에 변수를 사용할 수 있게 해준다
-//
-//
+// * > 모든 걸 대체할 수 있음을 뜻함  > 'ab*cd'  > 모두가능
+// + > 앞글자가 계속 반복되도 된다는 것을 뜻함 > 'ab+cd'   > 앞글자 반복
+// ? > 앞글자가 있어도 되고 없어도 된다는 것을 뜻함 > 'ab?cd' > 앞글자 선택사항
+// (ab)? > 묶인 그룹이 있어도 되고 없어도 됨 > 'ab(cd)?e > 그룹 선택사항
+// w > 아무 문자라는 뜻 > (nico\w+)
+// d > 숫자 라는 뜻 ? > (\\d+)
