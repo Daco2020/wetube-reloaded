bc1c5566a565ab103c8e7c87791943809521d6f3
Daco <dacokim32@gmail.com>
2021-06-22 07:41:01 +0900

    #5.9 Mixins

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,5 +1,30 @@
 export const trending = (req, res) => {
-  const videos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const videos = [
+    {
+      title: "First Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "Second Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "Third Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+  ];
   return res.render("home", { pageTitle: "Home", videos });
 };
 export const see = (req, res) => res.render("watch");
@@ -7,6 +32,3 @@ export const edit = (req, res) => res.render("Edit");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => res.send("Delete Video");
-
-// 개별 변수를 익스포트 할 때는 임포트 할 때도 개별 이름을 정확히 기재해주어야 함.
-// 디폴트 익스포트는 해당 파일에 하나밖에 없기 때문에 이름 변경 가능
